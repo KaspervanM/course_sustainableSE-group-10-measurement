b0cbebff482ab56502f6d675d665c83cc29a7dd4
Kasper van Maasdam <K.vanMaasdam@student.tudelft.nl>
2026-02-18 21:08:46 +0100

    chore: simplify nix files

--- shell.nix
+++ shell.nix
@@ -3,7 +3,30 @@
 }:
 
 let
-  energibridge = import ./energibridge.nix { };
+  energibridge = pkgs.rustPlatform.buildRustPackage rec  {
+  pname = "energibridge";
+  version = "main";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "tdurieux";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-LGOgs0oOVZGvAuQzDMGgLItF6qahthco7RlwTaS7piI=";
+  };
+
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+  };
+
+  buildPhase = ''
+    cargo build --release
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp target/release/energibridge $out/bin/
+  '';
+};
 in
 pkgs.mkShell {
   buildInputs = [ energibridge ];
