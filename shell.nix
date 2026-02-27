9696d93ab1f754ec59c27118cbbde0473007e6a1
Kasper van Maasdam <K.vanMaasdam@student.tudelft.nl>
2026-02-27 14:28:53 +0100

    chore: analize new data

--- shell.nix
+++ shell.nix
@@ -1,38 +1,42 @@
 {
-  pkgs ? import <nixpkgs> {},
+  pkgs ? import <nixpkgs> { },
 }:
 
 let
-  energibridge = pkgs.rustPlatform.buildRustPackage rec  {
-  pname = "energibridge";
-  version = "main";
+  energibridge = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "energibridge";
+    version = "main";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "tdurieux";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-LGOgs0oOVZGvAuQzDMGgLItF6qahthco7RlwTaS7piI=";
+    src = pkgs.fetchFromGitHub {
+      owner = "tdurieux";
+      repo = pname;
+      rev = version;
+      sha256 = "sha256-LGOgs0oOVZGvAuQzDMGgLItF6qahthco7RlwTaS7piI=";
+    };
+
+    cargoLock = {
+      lockFile = "${src}/Cargo.lock";
+    };
+
+    buildPhase = ''
+      cargo build --release
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp target/release/energibridge $out/bin/
+    '';
   };
-
-  cargoLock = {
-    lockFile = "${src}/Cargo.lock";
-  };
-
-  buildPhase = ''
-    cargo build --release
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp target/release/energibridge $out/bin/
-  '';
-};
 in
 pkgs.mkShell {
-  buildInputs = [ energibridge pkgs.jq ];
+  buildInputs = [
+    energibridge
+    pkgs.jq
+    pkgs.python3
+    pkgs.python3Packages.jupyter
+  ];
 
   shellHook = ''
-    chmod +x setup.sh test.sh src/gen_sequence.sh src/docker_test.sh src/podman_test.sh
     ./setup.sh
     exit
   '';
